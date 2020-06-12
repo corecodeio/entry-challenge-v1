0c33126cf16350c6d99a8a36d3a8be30a9affa80
Gus <gus@core-code.io>
2020-06-12 03:52:27 +0200

    Adds error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import Ajv from "ajv";
 import { compare, genSalt, hash } from "bcryptjs";
 import express from "express";
 import fs from "fs";
+import schema from "./schema.json";
 import server from "./server";
 
 export const router = express.Router({
@@ -8,6 +10,9 @@ export const router = express.Router({
 });
 
 server.get("/:emailAddress", async (req: Request, res: Response) => {
+  const message =
+    "Acceso inválido mi chavo. ¿Proporcionaste la contraseña en el parámetro x-password de los headers?";
+
   try {
     const password = req.headers["x-password"];
 
@@ -26,10 +31,11 @@ server.get("/:emailAddress", async (req: Request, res: Response) => {
       res.status(400);
       return res.json({
         status: "400",
-        message:
-          "Acceso inválido mi chavo. ¿Proporcionaste la contraseña en el parámetro x-password de los headers?",
+        message,
       });
     }
+
+    delete req.body.credentials;
 
     data.bienvenido = {
       claveDeAcceso: process.env.CLAVE_DE_ACCESO,
@@ -38,28 +44,40 @@ server.get("/:emailAddress", async (req: Request, res: Response) => {
     return res.json(data);
   } catch (error) {
     res.status(400);
-    return res.json({ status: "400", message: "Acceso inválido mi chavo..." });
+    return res.json({ status: "400", message });
   }
 });
 
 server.post("/", async (req: Request, res: Response) => {
-  const fileName = req.body?.contactInfo?.emailAddress;
+  const ajv = new Ajv();
+  try {
+    if (!ajv.validate(schema, req.body)) {
+      res.status(400);
+      return res.json({ status: "400", message: "La información está incompleta." });
+    }
 
-  req.body.credentials.password = await hash(
-    req.body.credentials.password,
-    await genSalt(),
-  );
+    const fileName = req.body?.contactInfo?.emailAddress;
 
-  fs.writeFile(
-    `./data/${fileName}.json`,
-    JSON.stringify(req.body, null, 2),
-    "utf8",
-    () => {},
-  );
+    req.body.credentials.password = await hash(
+      req.body.credentials.password,
+      await genSalt(),
+    );
 
-  delete req.body.credentials;
+    fs.writeFile(
+      `./data/${fileName}.json`,
+      JSON.stringify(req.body, null, 2),
+      "utf8",
+      () => {},
+    );
 
-  return res.json(req.body);
+    delete req.body.credentials;
+
+    return res.json(req.body);
+  } catch (error) {
+    console.error(error);
+    res.status(400);
+    res.json({ status: "400", message: "Por favor revisa la información proporcionada." });
+  }
 });
 
 server.listen("80", () => {
