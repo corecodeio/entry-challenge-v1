4933dc5d64c02a624977c902a702d18cb6a97d7d
Ktoxcon <kennethtoxro@gmail.com>
2020-06-13 17:31:10 -0600

    Fixes index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import fs from "fs";
 import schema from "./schema.json";
 import server from "./server";
+import { Request , Response } from 'express';
 import { sendInformation, validateInformation } from './client';
 
 export const router = express.Router({
@@ -34,10 +35,7 @@ server.get("/:emailAddress", async (req: Request, res: Response) => {
 
     if (!isValidPassword) {
       res.status(400);
-      return res.json({
-        status: "400",
-        message,
-      });
+      return res.json();
     }
 
     delete req.body.credentials;
