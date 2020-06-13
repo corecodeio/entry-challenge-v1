dc3efb859c6ed45b35facf0c043d12718b335464
areyes107 <angelmrg2000@gmail.com>
2020-06-13 17:33:52 -0600

    src/index:

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import express from "express";
 import fs from "fs";
 import schema from "./schema.json";
 import server from "./server";
+import { sendInformation, showInformation } from "./client";
+import {Request, Response} from "express";
 
 export const router = express.Router({
   strict: true,
@@ -90,3 +92,6 @@ server.post("/", async (req: Request, res: Response) => {
 server.listen("80", () => {
   console.log("listening");
 });
+
+sendInformation(); 
+showInformation();
