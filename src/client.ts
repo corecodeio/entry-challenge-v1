5cc2c4d90a8d0abde68a6d1a9ccfcc9e7de68f28
Lucas Solares <solareslucas.figueroa@gmail.com>
2020-06-14 16:34:30 -0600

    Implements POST Request with the schema

---
+++ src/client.ts
@@ -0,0 +1,45 @@
+import { default as Axios } from "axios";
+import * as dotEnv from "dotenv";
+
+dotEnv.config();
+
+const sendSchema = async () => {
+  try {
+    await Axios.post("http://localhost:80", {
+      contactInfo: {
+        fullName: "Lucas Guillermo Solares Figueroa",
+        emailAddress: "solareslucas.figueroa@gmail.com",
+      },
+      github: {
+        profileURL: "https://github.com/LucasSolares",
+        username: "LucasSolares",
+      },
+      credentials: {
+        password: process.env.PASSWORD,
+      },
+      personalInfo: {
+        questions: [
+          {
+            question: "If I was a Sr. Programmer, I would like to build:",
+            answer: "I would like to found a technology company worldwide.",
+          },
+          {
+            question:
+              "Por favor indica el URL que me lleva a la línea de código de la definición de React.useEffect",
+            answer:
+              "https://github.com/facebook/react/blob/655affa302437208e6f03c9ca6d170ea1707ace3/packages/react-reconciler/src/ReactFiberHooks.new.js#L1878",
+          },
+          {
+            question: "code is poetry, because:",
+            answer:
+              "The structure of the code must be clean and beautiful like that of poetry.",
+          },
+        ],
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+sendSchema();
