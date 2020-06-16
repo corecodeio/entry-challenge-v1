25427ca26f626b6a7431b0ee941e49fe6e7574b3
Lucas Solares <solareslucas.figueroa@gmail.com>
2020-06-16 10:53:30 -0600

    Remove package-lock and create data file

--- src/client.ts
+++ src/client.ts
@@ -3,40 +3,13 @@ import * as dotEnv from "dotenv";
 
 dotEnv.config();
 
+import data from './data';
+
+
 const sendSchema = async () => {
   try {
-    const response = await Axios.post(process.env.HOST, {
-      contactInfo: {
-        fullName: "Lucas Guillermo Solares Figueroa",
-        emailAddress: "solareslucas.figueroa@gmail.com",
-      },
-      github: {
-        profileURL: "https://github.com/LucasSolares",
-        username: "LucasSolares",
-      },
-      credentials: {
-        password: process.env.PASSWORD,
-      },
-      personalInfo: {
-        questions: [
-          {
-            question: "If I was a Sr. Programmer, I would like to build:",
-            answer: "I would like to found a technology company worldwide.",
-          },
-          {
-            question:
-              "Por favor indica el URL que me lleva a la línea de código de la definición de React.useEffect",
-            answer:
-              "https://github.com/facebook/react/blob/655affa302437208e6f03c9ca6d170ea1707ace3/packages/react-reconciler/src/ReactFiberHooks.new.js#L1878",
-          },
-          {
-            question: "code is poetry, because:",
-            answer:
-              "The structure of the code must be clean and beautiful like that of poetry.",
-          },
-        ],
-      },
-    });
+    console.log(data);
+    const response = await Axios.post(process.env.HOST, data);
     console.log({
       status: response.status,
       data: response.data,
