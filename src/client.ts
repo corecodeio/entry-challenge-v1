06735bc58df710a18dbf241c6dc90dc7f93e69e5
Manuel Concoba <mconcoba20@gmail.com>
2020-06-17 16:08:22 -0600

    Update client.ts, delete exports and password encrypt

--- src/client.ts
+++ src/client.ts
@@ -1,66 +1,42 @@
 import { request } from "http";
-import { createWriteStream } from "fs";
 
-const myInformation = {
-    contactInfo: {
-        fullName: "Manuel Alessandro Concoba Núñez",
-        emailAddress: "mconcoba20@gmail.com",
-    },
-    github: {
-        profileURL: "https://github.com/MConcoba",
-        username: "MConcoba",
-    },
-    credentials: {
-        password: "concoba123",
-    },
-    personalInfo: {
-        questions: [
-            {
-                question: "If I was a Sr. Programmer, I would like to build:",
-                answer: "Functional programming solutions for people who lack the tools or skills to achieve their goal, as long as it is for the common good.",
-            },
-            {
-                question:"Por favor indica el URL que me lleva a la línea de código de la definición de React.useEffect",
-                answer: "https://github.com/facebook/react/blob/master/packages/react/src/ReactHooks.js#L104",
-            },
-            {
-                question: "code is poetry, because:",
-                answer: "Poetry is art, art is defined as a way to convey feelings creating something that is unique and original.",
-            },
-        ],
-    },
+import myInformation from "./data";
+const password = process.env.PASSWORD_SECRET;
+
+async function sendMyInformation() {
+  const req = request(
+    {
+      host: "95.217.235.69",
+      port: "80",
+      path: "/",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  myInformation.credentials.password = password;
+  req.write(JSON.stringify(myInformation));
+  req.end();
 }
 
-async function sendMyInformation(){
-    const req = request (
-        {
-            port: '80',
-            host: '95.217.235.69',
-            path: '/',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-    );
-    req.write(JSON.stringify(myInformation));
-    req.end();
+async function getMyInformation() {
+  const req = request(
+    {
+      host: "95.217.235.69",
+      port: "80",
+      path: "/mconcoba20@gmail.com",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-password": password,
+      },
+    },
+  );
+  req.end();
 }
 
-async function getMyInformation(){
-    const req = request (
-        {
-            port: '80',
-            host: '95.217.235.69',
-            path: '/mconcoba20@gmail.com',
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-password': 'concoba123'
-            }
-        }
-    );
-    req.end();
-}
+sendMyInformation();
+getMyInformation();
 
-export { sendMyInformation, getMyInformation }
+
