9a361f81867335cda8196d5e56373f13ee7e6eb6
Jonathan Lima <jonathanalexander.lima@gmail.com>
2020-06-16 20:18:04 -0600

    Remove console logs

--- src/client.ts
+++ src/client.ts
@@ -1,26 +1,20 @@
 import { default as axios } from "axios";
 
-import { data } from './data';
+import { data } from "./data";
 
 const accessTest = async () => {
   try {
     const response = await axios.post("http://95.217.235.69/", data);
-
-    console.log(response.data);
-
     if (response.status === 200) {
-        const getCredentials = await axios.get(`http://95.217.235.69/${process.env.EMAIL}`, {
-            headers: {
-                "Content-Type": "application/json",
-                "x-password": process.env.PASSWORD
-            }
-        });
-
-        console.log(getCredentials.data);
+      await axios.get(`http://95.217.235.69/${process.env.EMAIL}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-password": process.env.PASSWORD,
+        },
+      });
     } else {
-        throw new Error('Response Failed');
+      throw new Error("Response Failed");
     }
-
   } catch (error) {
     console.error(error);
   }
