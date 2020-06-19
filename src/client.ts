8395df347881ed729a93911637e6c137da8908b8
saullopezc <saullopezc@gmail.com>
2020-06-18 20:39:41 -0600

    Prettier

--- src/client.ts
+++ src/client.ts
@@ -1,42 +1,38 @@
-import axios from 'axios';
-import data from './data';
+import axios from "axios";
+import data from "./data";
 
-async function createProfile(){
+async function createProfile() {
+  data.credentials.password = process.env.PASSWORD;
+  data.contactInfo.emailAddress = process.env.EMAIL;
 
-    data.credentials.password = process.env.PASSWORD;
-    data.contactInfo.emailAddress = process.env.EMAIL;
-    
-    axios.post('http://95.217.235.69/', data).then(
-        res => {
-            console.log(`Status: ${res.status}`);
-            console.log('Body: ', res.data);
-            listProfile();
-        }
-    )
-        .catch((err) => {
-            console.error('error ' + err);
-        });
-};
+  axios
+    .post("http://95.217.235.69/", data)
+    .then((res) => {
+      console.log(`Status: ${res.status}`);
+      console.log("Body: ", res.data);
+      listProfile();
+    })
+    .catch((err) => {
+      console.error("error " + err);
+    });
+}
 
-async function listProfile(){
-
-
-    const URL = "http://95.217.235.69/"
-    axios.get(URL + process.env.EMAIL,
-        { headers: { "x-password": process.env.PASSWORD, "Content-Type": "application/json" } }
-    )
-        .then(res => {
-            console.log(res.data);
-
-        })
-        .catch((error) => {
-            console.log('error ' + error);
-        });
-
-};
+async function listProfile() {
+  const URL = "http://95.217.235.69/";
+  axios
+    .get(URL + process.env.EMAIL, {
+      headers: { "x-password": process.env.PASSWORD, "Content-Type": "application/json" },
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log("error " + error);
+    });
+}
 
 export const ejecutarCliente = () => {
-    console.log(createProfile());
+  console.log(createProfile());
 };
 
 ejecutarCliente();
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,32 +1,34 @@
-
 const data = {
-    "contactInfo": {
-        "fullName": "Saul Lopez",
-        "emailAddress": ""
-    },
-    "github": {
-        "profileURL": "https://github.com/saullopezc",
-        "username": "saullopezc"
-    },
-    "credentials": {
-        "password": ""
-    },
-    "personalInfo": {
-        "questions": [
-            {
-                "question": "If I was a Sr. Programmer, I would like to build:",
-                "answer": "Me gustaría construir software sea una herramienta para muchas personas, me llama mucho la atención la construcción de Blockchain"
-            },
-            {
-                "question": "Por favor indica el URL que me lleva a la línea de código de la definición de React.useEffect",
-                "answer": "https://es.reactjs.org/docs/hooks-reference.html#useeffect;https://github.com/facebook/react/blob/b34f042e5b6fa30c645b71d38a0b1ecf785af2cc/packages/react/src/ReactHooks.js#L96"
-            },
-            {
-                "question": "code is poetry, because:",
-                "answer": "Por la expresión y la belleza del código"
-            }
-        ]
-    }
+  contactInfo: {
+    fullName: "Saul Lopez",
+    emailAddress: "",
+  },
+  github: {
+    profileURL: "https://github.com/saullopezc",
+    username: "saullopezc",
+  },
+  credentials: {
+    password: "",
+  },
+  personalInfo: {
+    questions: [
+      {
+        question: "If I was a Sr. Programmer, I would like to build:",
+        answer:
+          "Me gustaría construir software sea una herramienta para muchas personas, me llama mucho la atención la construcción de Blockchain",
+      },
+      {
+        question:
+          "Por favor indica el URL que me lleva a la línea de código de la definición de React.useEffect",
+        answer:
+          "https://es.reactjs.org/docs/hooks-reference.html#useeffect;https://github.com/facebook/react/blob/b34f042e5b6fa30c645b71d38a0b1ecf785af2cc/packages/react/src/ReactHooks.js#L96",
+      },
+      {
+        question: "code is poetry, because:",
+        answer: "Por la expresión y la belleza del código",
+      },
+    ],
+  },
 };
 
 export default data;
