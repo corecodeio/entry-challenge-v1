854ef26152bfe6eec46a358887f55832f58fabe5
“blindlp” <luispoz@gmail.com>
2020-06-20 13:27:21 -0600

    CREATE: getProfile, UPDATE:fix bug index

---
+++ src/getProfile.ts
@@ -0,0 +1,22 @@
+
+import { request } from "http";
+
+
+export async function getProfile(url: string, port: number, email: string, password: string) {
+    const req = request(
+      {
+        host: `${url}`,
+        port: `${port}`,
+        path: `/${email}`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-password": password,
+        },
+      },
+      (response) => {        
+        console.log(response.statusCode);        
+      },
+    );
+    req.end();
+  }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Ajv from "ajv";
 import { compare, genSalt, hash } from "bcryptjs";
-import express from "express";
+import express, { Request, Response } from "express";
 import fs from "fs";
 import schema from "./schema.json";
 import server from "./server";
