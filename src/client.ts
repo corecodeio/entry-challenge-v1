331fdef85d53bc90aea621cfbd4b9457774e4e15
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2020-06-19 22:47:08 -0600

    Se aplica prettier

--- src/client.ts
+++ src/client.ts
@@ -1,53 +1,42 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 import { data } from "./data";
 
+async function postData() {
+  const url = "http://95.217.235.69/";
 
-async function postData(){
+  const postParamethers = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
 
-    const url='http://95.217.235.69/';
-
-    const postParamethers ={
-        method:"POST",
-        headers:{
-            "Content-Type": "application/json"
-        },
-        body:  JSON.stringify(data)
-    }
-
-    try {
-        let response = await fetch(url,postParamethers);
-        let result = await response.json();
-      } catch (error) {
-        
-      }
-    
+  try {
+    let response = await fetch(url, postParamethers);
+    let result = await response.json();
+  } catch (error) {}
 }
 
+async function getData() {
+  const url = "http://95.217.235.69/";
 
-async function getData(){
-
-    const url='http://95.217.235.69/';
-
-    const getParamethers ={
+  const getParamethers = {
     method: "GET",
     headers: {
-        "Content-Type": "application/json",
-        "x-password": data.credentials.password
-        }    
-    }
+      "Content-Type": "application/json",
+      "x-password": data.credentials.password,
+    },
+  };
 
-    const email = data.contactInfo.emailAddress;
-    
-    try {
-        let response = await fetch(url+email,getParamethers);
-        let data = await response.json();
-      } catch (error) {
-        
-      }
+  const email = data.contactInfo.emailAddress;
+
+  try {
+    let response = await fetch(url + email, getParamethers);
+    let data = await response.json();
+  } catch (error) {}
 }
 
 postData();
 
 getData();
-
-
