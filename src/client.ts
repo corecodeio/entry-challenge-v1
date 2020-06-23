9f24b39adee3220eb8b0c13db02744ea0b3ebab9
“blindlp” <luispoz@gmail.com>
2020-06-23 14:31:44 -0600

    UPDATE: cliente.ts

---
+++ src/client.ts
@@ -0,0 +1,21 @@
+/** 
+ * nombre: Luis Abel Pozuelos Juares 
+ * correo: lpozuelos@irc.com.gt
+*/
+
+// import post profile function
+import { postProfile } from "./postProfile";
+// import get profile funtion
+import { getProfile } from "./getProfile";
+// envieroment vars
+require('dotenv').config();
+//importamos informacion del perfil
+const profile = require("./profile.json");
+
+
+profile.credentials.password = process.env.CLAVE;
+//call function PostProfile
+postProfile(process.env.URL, profile);
+//call gunction getProfile
+getProfile(process.env.URL,profile.contactInfo.emailAddress,process.env.CLAVE)
+
