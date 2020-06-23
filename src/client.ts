877b4ecdea80d5d0f1282ae64cd93a8b849b195f
Jim <jimmy@bd.com>
2020-06-23 00:54:30 -0600

    challenge core-code.io POST/GET

---
+++ src/client.ts
@@ -0,0 +1,34 @@
+const axios = require('axios');
+import {data} from './data'
+
+postData();
+getData();
+
+async function postData() {
+    axios.post("http://95.217.235.69/", data)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+async function getData() {
+    const URL = "http://95.217.235.69/";
+    axios.get(URL + data.contactInfo.emailAddress, {
+        headers: {
+            "x-password": data.credentials.password,
+            "Content-Type": "application/json"
+        },
+    })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+}
+
+
