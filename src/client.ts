be79694caede0d1b76d3871ef5b22892d7bfa3d7
Jim <jimmy@bd.com>
2020-06-24 21:24:49 -0600

    Import - dotenv - prettier

--- src/client.ts
+++ src/client.ts
@@ -1,34 +1,33 @@
-const axios = require('axios');
-import {data} from './data'
+import axios from "axios";
+import { data } from "./data";
 
 postData();
 getData();
 
 async function postData() {
-    axios.post("http://95.217.235.69/", data)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  axios
+    .post("http://95.217.235.69/", data)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 async function getData() {
-    const URL = "http://95.217.235.69/";
-    axios.get(URL + data.contactInfo.emailAddress, {
-        headers: {
-            "x-password": data.credentials.password,
-            "Content-Type": "application/json"
-        },
+  const URL = "http://95.217.235.69/";
+  axios
+    .get(URL + data.contactInfo.emailAddress, {
+      headers: {
+        "x-password": data.credentials.password,
+        "Content-Type": "application/json",
+      },
     })
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
-
-
