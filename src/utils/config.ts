e76f49d5135caaaeb417a9367d8853c6110aaf8c
Jim <jimmy@bd.com>
2020-06-28 11:07:07 -0600

    dontenv config.ts - .env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,7 @@
 import * as dotenv from "dotenv";
 
 dotenv.config();
-let path;
-switch (process.env.NODE_ENV) {
-  case "production":
-    path = `${__dirname}/../../.env.production`;
-    break;
-  default:
-    path = `${__dirname}/../../.env.development`;
-}
+let path = `${__dirname}/../../.env`;
 dotenv.config({ path: path });
 
 export const PASSWORD_ENV = process.env.PASSWORD_ENV;
